0584d000045785ab54a8a7fd90880cb0609bee38
Jun Yong <tombyong@gmail.com>
2025-04-22 10:38:13 +0800

    update columns in one statement instead

--- src/main/java/com/tsd/workshop/transaction/data/WorkshopServiceSqlRepository.java
+++ src/main/java/com/tsd/workshop/transaction/data/WorkshopServiceSqlRepository.java
@@ -22,36 +22,28 @@ public class WorkshopServiceSqlRepository {
                 .bind(0, id)
                 .flatMap(Result::getRowsUpdated)
                 .singleOrEmpty()
+                .switchIfEmpty(Mono.just(0L))
                 .map(count -> {
                     if (count != 1L) throw new IncorrectResultSizeDataAccessException("expected one but %s".formatted(count), 1);
                     return count;
                 })
                 .flatMap(insertCount ->
-                        databaseClient.sql("update deleted_workshop_service set deletion_date = :deletion_date where id = :id")
+                        databaseClient.sql("""
+                                        update deleted_workshop_service set deletion_date = :deletion_date,
+                                        spare_part_usages =
+                                        (select json_agg(row_to_json(spu)) from spare_part_usages spu where service_id = :service_id)
+                                        where id = :id
+                                        """)
                                 .bind(0, LocalDate.now())
                                 .bind(1, id)
+                                .bind(2, id)
                                 .flatMap(Result::getRowsUpdated)
+                                .singleOrEmpty()
+                                .switchIfEmpty(Mono.just(0L))
                                 .flatMap(updateCount -> {
                                     if (!updateCount.equals(insertCount)) throw new IncorrectResultSizeDataAccessException("expected %s but %s"
                                             .formatted(insertCount, updateCount), insertCount.intValue());
                                     return Mono.just(updateCount);
-                                })
-                                .singleOrEmpty()
-                )
-                .flatMap(updateCount ->
-                        databaseClient.sql("""
-                                        update deleted_workshop_service set spare_part_usages =
-                                        (select json_agg(row_to_json(spu)) from spare_part_usages spu where service_id = :service_id)
-                                        where id = :id
-                                        """)
-                                .bind(0, id)
-                                .bind(1, id)
-                                .flatMap(Result::getRowsUpdated)
-                                .singleOrEmpty()
-                                .flatMap(jsonUpdateCount -> {
-                                    if (!jsonUpdateCount.equals(updateCount)) throw new IncorrectResultSizeDataAccessException("expected %s but %s"
-                                            .formatted(updateCount, jsonUpdateCount), updateCount.intValue());
-                                    return Mono.just(jsonUpdateCount);
                                 })
                 );
     }
