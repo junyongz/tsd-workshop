39c7aba215423a02ff8579141c85671a78c12df9
Jun Yong <tombyong@gmail.com>
2025-05-22 09:04:58 +0800

    further for orders used by non-complete servce

--- src/main/java/com/tsd/workshop/migration/suppliers/data/SupplierSparePartRepository.java
+++ src/main/java/com/tsd/workshop/migration/suppliers/data/SupplierSparePartRepository.java
@@ -8,7 +8,8 @@ public interface SupplierSparePartRepository extends R2dbcRepository<SupplierSpa
 
     @Query("""
             select * from mig_supplier_spare_parts spp where exists
-            (select 1 from spare_part_usages where order_id = spp.id)
+            (select 1 from spare_part_usages spu where order_id = spp.id
+            and (select completion_date from workshop_service where id = spu.service_id) is null)
             order by invoice_date desc, delivery_order_no desc
             """)
     Flux<SupplierSparePart> findWithUsageSupplierSparePartsBy();
